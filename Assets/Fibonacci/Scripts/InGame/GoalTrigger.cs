894b3be2cbd1c2e8c461fbcc784bbb8b7a18214d
YutoImata <yuto.imata.wine@gmail.com>
2025-12-20 23:02:52 +0900

    使用していないメソッドなどを削除

--- Assets/Fibonacci/Scripts/InGame/GoalTrigger.cs
+++ Assets/Fibonacci/Scripts/InGame/GoalTrigger.cs
@@ -1,27 +1,18 @@
 using Fibonacci.Scene;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 /// <summary>
 /// プレイヤーがゴールに到達した時の判定とシーン遷移を管理するクラス
 /// </summary>
 public class GoalTrigger : MonoBehaviour
 {
-    [Header("ゴールの設定")]
-    [SerializeField] private string targetSceneName = "Title";
-
     /// <summary>
     /// プレイヤーがゴールに到達した時の判定とシーン遷移を管理するクラス
     /// </summary>
     private void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log("Goal Trigger 2D activated");
-        // Playerタグのオブジェクトと接触したかチェック
         if (other.CompareTag("Player"))
-        {
-            Debug.Log("Goal reached! Loading Result scene...");
             LoadResultScene();
-        }
     }
 
     private void LoadResultScene()
