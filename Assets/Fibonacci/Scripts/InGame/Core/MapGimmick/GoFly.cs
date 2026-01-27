d442acd7627727647d18ac7650fc53f3876ba3ae
NEKOMARU23 <nekosuki29@icloud.com>
2026-01-27 09:50:21 +0900

    feat:垂直にしか飛ばないのを斜めに飛べるように変更

--- Assets/Fibonacci/Scripts/InGame/Core/MapGimmick/GoFly.cs
+++ Assets/Fibonacci/Scripts/InGame/Core/MapGimmick/GoFly.cs
@@ -8,6 +8,8 @@ namespace Fibonacci.InGame.Core.MapGimmick
         [Header("吹っ飛ばし設定")]
         [SerializeField] private float launchForce = 15f; 
         [SerializeField] private ParticleSystem launchEffect;
+        [Range(0f, 1f)]
+        [SerializeField] private float upwardBias = 0.7f; // 上方向への強さの割合
 
         private void OnTriggerEnter2D(Collider2D other)
         {
@@ -22,9 +24,13 @@ namespace Fibonacci.InGame.Core.MapGimmick
             var rb = player.GetComponent<Rigidbody2D>();
             if (rb != null)
             {
+                float moveDirection = rb.linearVelocity.x >= 0 ? 1f : -1f;
+
+                Vector2 launchVector = new Vector2(moveDirection, upwardBias).normalized;
+
                 rb.linearVelocity = new Vector2(rb.linearVelocity.x, 0);
 
-                rb.AddForce(transform.up * launchForce, ForceMode2D.Impulse);
+                rb.AddForce(launchVector * launchForce, ForceMode2D.Impulse);
             }
 
             if (launchEffect != null)
