0671f3c078f8a2c6bd5e76c0676bd7f138303437
NEKOMARU23 <nekosuki29@icloud.com>
2026-01-28 12:04:33 +0900

    refactor:PlayerInputManagerの整理

--- Assets/Fibonacci/Scripts/InGame/Player/PlayerInputManager.cs
+++ Assets/Fibonacci/Scripts/InGame/Player/PlayerInputManager.cs
@@ -7,8 +7,7 @@ namespace Fibonacci.InGame.Player
 {
     /// <summary>
     /// 入力デバイスからの信号をゲーム内コマンドへ変換する管理クラス。
-    /// リスタート時には司令塔として、ゲームフェーズの差し戻し、能力のリセット、
-    /// およびプレイヤー状態の初期化を適切な順序で実行します。
+    /// リスタート時には司令塔として、ゲームフェーズ、能力、プレイヤー状態を初期化します。
     /// </summary>
     public class PlayerInputManager : MonoBehaviour
     {
@@ -30,29 +29,26 @@ namespace Fibonacci.InGame.Player
         }
 
         /// <summary>
-        /// 移動入力（スティック/キーボード）を検知し、プレイヤーコントローラーへベクトルを伝達します。
+        /// 移動入力を検知し、プレイヤーコントローラーへベクトルを伝達します。
         /// </summary>
-        /// <param name="context">InputSystemのアクションコンテキスト</param>
         public void OnMove(InputAction.CallbackContext context)
         {
-            if (playerController != null)
-            {
-                playerController.SetMoveInput(context.ReadValue<Vector2>());
-            }
+            playerController.SetMoveInput(context.ReadValue<Vector2>());
         }
 
+        /// <summary>
+        /// ジャンプ入力を検知し、コントローラーへ通知します。
+        /// </summary>
         public void OnJump(InputAction.CallbackContext context)
         {
-            if (context.started && playerController != null)
-            {
-                playerController.OnJumpInput();
-            }
+            if (!context.started) return;
+            
+            playerController.OnJumpInput();
         }
 
         /// <summary>
         /// リスタートボタンの入力を検知し、システム全体へリスタートイベントを発火させます。
         /// </summary>
-        /// <param name="context">InputSystemのアクションコンテキスト</param>
         public void OnRestart(InputAction.CallbackContext context)
         {
             if (context.started)
@@ -64,15 +60,13 @@ namespace Fibonacci.InGame.Player
         /// <summary>
         /// 所属エリアの変更を記録します。
         /// </summary>
-        /// <param name="newAreaIndex">進入したエリアのインデックス</param>
         public void OnAreaChanged(int newAreaIndex)
         {
             lastAreaIndex = newAreaIndex;
         }
 
         /// <summary>
-        /// システム全体のリスタート要求に応じ、依存する各マネージャーとオブジェクトを初期化します。
-        /// 物理演算の予期せぬ挙動を防ぐため、最初にゲームフェーズを停止状態（Drawing）へリセットします。
+        /// システム全体のリスタート要求に応じ、関連するマネージャーとオブジェクトを初期設定に戻します。
         /// </summary>
         private void OnGameRestart()
         {
@@ -81,15 +75,15 @@ namespace Fibonacci.InGame.Player
                 GameManager.Instance.ResetPhase();
             }
 
-            AbilityManager.Instance.Reset();
+            if (AbilityManager.Instance != null)
+            {
+                AbilityManager.Instance.Reset();
+            }
 
             EffectIdArea0 = "";
             EffectIdArea1 = "";
 
-            if (playerController != null)
-            {
-                playerController.ResetPlayerState();
-            }
+            playerController.ResetPlayerState();
 
             lastAreaIndex = -1;
         }
