f30d4467d44fd64653e0b1b2b774f55370b01755
NEKOMARU23 <nekosuki29@icloud.com>
2026-01-29 12:50:33 +0900

    feat:定数のみのクラスの作成

---
+++ Assets/Fibonacci/Scripts/InGame/Player/GameConstants.cs
@@ -0,0 +1,13 @@
+namespace Fibonacci.InGame.Player
+{
+    /// <summary>
+    /// ゲーム全体で使用されるタグ名、レイヤー名、および共通の数値を管理する定数保持クラス。
+    /// </summary>
+    public static class GameConstants
+    {
+        public const string TAG_PLAYER = "Player";
+        
+        public const float DIRECTION_RIGHT = 1f;
+        public const float DIRECTION_LEFT = -1f;
+    }
+}
